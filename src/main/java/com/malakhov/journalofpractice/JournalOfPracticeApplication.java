fac645cbe81e326627737e372eca65f454952f31
Yevhenii Malakhov <malakhov747@gmail.com>
2023-02-16 21:51:21 +0200

    Add SpringBoot dependency

---
+++ src/main/java/com/malakhov/journalofpractice/JournalOfPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.malakhov.journalofpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalOfPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournalOfPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malakhov/journalofpractice/JournalOfPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malakhov.journalofpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalOfPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
